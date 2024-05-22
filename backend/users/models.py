8ea55c70c375faca05cfc18a002c9baf09b7dffd
Haro Mikhaylov <www.haris@bk.ru>
2024-05-22 20:38:48 +0300

    Alter superuser creation, alter default superuser role field to admin

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,20 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import UniqueConstraint, CheckConstraint, Q, F
+
+
+class ProfileManager(BaseUserManager):
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', Profile.ADMIN)
+
+        email = self.normalize_email(email)
+        superuser = self.model(email=email, **extra_fields)
+        superuser.set_password(password)
+        superuser.save(using=self._db)
+        return superuser
 
 
 class Profile(AbstractUser):
@@ -25,6 +39,11 @@ class Profile(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = ProfileManager()
+
+    def __str__(self):
+        return self.username
 
 
 class Subscription(models.Model):
