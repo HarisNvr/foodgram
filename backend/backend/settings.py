fee49a998b865fa48a0e58f059304f4ed0ebce78
HarisNvr <yc-user@epdi49ikn4hclnk8a55f.auto.internal>
2024-06-05 15:42:01 +0000

    First server commit!

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,8 +71,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
