fe6992afb13c7f6e290fe307855043b7710a0249
Haro Mikhaylov <www.haris@bk.ru>
2024-06-07 12:38:12 +0300

    1-st rev after minor v5 "api" work

--- backend/recipes/management/commands/importcsv.py
+++ backend/recipes/management/commands/importcsv.py
@@ -4,12 +4,13 @@ import os
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
     models_files = {
         Ingredient: 'ingredients.csv',
+        Tag: 'tags.csv',
     }
 
     def handle(self, *args, **options):
@@ -19,16 +20,21 @@ class Command(BaseCommand):
 
     def import_data(self, model, filename):
         try:
+            self.stdout.write(self.style.WARNING(f'Старт импорта {filename}'))
             with open(filename, encoding='utf-8') as file:
                 reader = csv.DictReader(file)
-                ingredient_id = 1
+                next_id = (
+                    model.objects.last().id + 1
+                    if model.objects.exists()
+                    else 1
+                )
                 for row in reader:
-                    if model == Ingredient:
+                    if not model.objects.filter(**row).exists():
                         model.objects.update_or_create(
-                            id=ingredient_id,
+                            id=next_id,
                             defaults=row
                         )
-                        ingredient_id += 1
+                        next_id += 1
             self.stdout.write(self.style.SUCCESS(f'Импортирован {filename}'))
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'Ошибка {filename}: {e}'))
